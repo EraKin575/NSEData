d6b70d3007828db33300920b40889a62f74e560c
EraKin575 <tejaskumar574@gmail.com>
2025-08-19 17:24:09 +0000

    bffb

--- internal/processing/processing.go
+++ internal/processing/processing.go
@@ -6,6 +6,7 @@ import (
 	csvwriter "server/internal"
 	"server/internal/api"
 	"server/models"
+	"strings"
 	"sync"
 	"time"
 )
@@ -14,14 +15,16 @@ func ProcessingOptionChain(records *[]models.Records, loc *time.Location, mu *sy
 	ticker := time.NewTicker(3 * time.Minute)
 	defer ticker.Stop()
 
+
 	csvWritten := false
+	var lastTimeStampRecorded string
 
 	for {
 		now := time.Now().In(loc)
 
-		startTime := time.Date(now.Year(), now.Month(), now.Day(), 9, 16, 0, 0, loc)
-		endTime := time.Date(now.Year(), now.Month(), now.Day(), 15, 31, 0, 0, loc)
-		resetTime := time.Date(now.Year(), now.Month(), now.Day(), 20, 0, 0, 0, loc)
+		startTime := time.Date(now.Year(), now.Month(), now.Day(), 9, 15, 0, 0, loc)
+		endTime := time.Date(now.Year(), now.Month(), now.Day(), 15, 40, 0, 0, loc)
+		resetTime := time.Date(now.Year(), now.Month(), now.Day(), 23, 55, 0, 0, loc)
 
 		switch {
 		case now.Before(startTime):
@@ -53,10 +56,17 @@ func ProcessingOptionChain(records *[]models.Records, loc *time.Location, mu *sy
 			continue
 		}
 
-		mu.Lock()
-		*records = append(*records, newRecords)
-		mu.Unlock()
+		// Checks timestamp before recording if the data is fresh(of the same date) based on timestamp and also takes care of duplicate data
+		if strings.Split(newRecords.TimeStamp, " ")[0] == time.Now().In(loc).Format("2006-01-02") && lastTimeStampRecorded != newRecords.TimeStamp {
+			mu.Lock()
+			*records = append(*records, newRecords)
+			mu.Unlock()
+			lastTimeStampRecorded = newRecords.TimeStamp
+		}
+
+		
 
 		<-ticker.C
 	}
+
 }
